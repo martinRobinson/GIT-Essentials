cc990a6013dcff57a4b727a5b55c2af46abdb6f7
Martin H. Robinson <martin.robinson@lyit.ie>
2018-02-22 15:33:06 +0000

    More documentation
    Added more documentation.

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,8 @@
 
 def my_square(x):
-    "" Takes a value and squares it.
+    """ Takes a value and squares it.
+        Uses the ** exponent operator.
+    """
     return (x**2)
 
 print (my_square(3))
